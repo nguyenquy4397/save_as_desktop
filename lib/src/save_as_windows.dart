d8855e7b144545132f192e3d3b49791cdad11b63
NguyenQuy <nguyenquy4397@gmail.com>
2022-05-16 15:07:48 +0700

    add feature open save dialog for windows

--- lib/src/save_as_windows.dart
+++ lib/src/save_as_windows.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:save_as_desktop/src/save_as_desktop.dart';
 
 class SaveAsWindows extends SaveAsDesktop {
@@ -7,8 +9,46 @@ class SaveAsWindows extends SaveAsDesktop {
       String? fileName,
       String? initialDirectory,
       String? extension,
-      bool lockParentWindow = false}) {
-    // TODO: implement saveFile
-    throw UnimplementedError();
+      bool lockParentWindow = false}) async {
+    String command = '''function Save-File([string] \$initialDirectory){
+
+          [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+      
+          \$OpenFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+          \$OpenFileDialog.initialDirectory = \$initialDirectory
+          \$OpenFileDialog.filter = "${extension != null ? '${extension.toUpperCase()} (*.$extension)| *.$extension' : 'All files (*.*)| *.*'}"
+          \$OpenFileDialog.ShowDialog() |  Out-Null
+      
+          return \$OpenFileDialog.filename
+        }
+
+        \$SaveFile=Save-File ${initialDirectory ?? '\$env:USERPROFILE'}
+      
+        if (\$SaveFile -ne "")
+        {
+        echo "You choose FileName: \$SaveFile"
+        } else {
+        echo "No File was chosen"
+        }''';
+
+    final processConvertBase64 = await Process.run(
+      'Powershell.exe',
+      [
+        '[System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes(\'$command\'))',
+      ],
+    );
+
+    final processSave = await Process.run(
+      'Powershell.exe',
+      [
+        '-EncodedCommand',
+        processConvertBase64.stdout,
+      ],
+    );
+    final path = processSave.stdout?.toString().trim();
+    if (processSave.exitCode != 0 || path == null || path.isEmpty) {
+      return null;
+    }
+    return path;
   }
 }
